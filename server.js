6bd43fc9214ccbbc4c9d7d9c1cec20ba99e95cf2
unknown <mrazvan99@yahoo.com>
2020-12-13 15:30:27 +0200

    full back-end with database for users

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 const cors = require('cors')
 const sequelize = new Sequelize('AnonymousGrading', 'root', 'root', {
-  dialect: 'mysql'
+  dialect: 'mysql',
+  
 })
 
 const app = express()
@@ -32,13 +33,35 @@ const User=sequelize.define('user',{
   }
 })
 
-const users=[]
+
 
 app.get('/create', async (req, res, next) => {
   try {
     await sequelize.sync({ force: true })
     res.status(201).json({ message: 'created' })
   } catch (err) {
+    next(err)
+  }
+})
+
+app.get('/users', async(req,res, next)=>{
+  try{
+    const users=await User.findAll()
+    res.status(200).json(users)
+  } catch(err){
+    next(err)
+  }
+})
+
+app.get('/users/:usr_id', async(req,res,next)=>{
+  try{
+    const user= await User.findByPk(req.params.usr_id)
+    if(user){
+      res.status(200).json(user)
+    }else{
+      res.status(404).json({message:'not found'})
+    }
+  }catch(err){
     next(err)
   }
 })
@@ -50,6 +73,34 @@ app.post('/users', async(req , res, next)=>{
   }catch(err){
     next(err)
   }
+})
+
+app.put('/users/:usr_id', async(req,res,next)=>{
+  try{
+  const user= await User.findByPk(req.params.usr_id)
+    if(user){
+      await user.update(req.body)
+      res.status(202).json({ message: 'accepted' })
+    }else{
+      res.status(404).json({message:'not found'})
+    }
+  }catch(err){
+    next(err)
+  }
+})
+
+app.delete('/users/:usr_id', async(req, res, next)=>{
+  try{
+    const user= await User.findByPk(req.params.usr_id)
+      if(user){
+        await user.destroy()
+        res.status(202).json({ message: 'accepted' })
+      }else{
+        res.status(404).json({message:'not found'})
+      }
+    }catch(err){
+      next(err)
+    }
 })
 
 app.listen(8080)
