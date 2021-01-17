255069a5bf1c48fc0a333153112d1ce8702f3eb2
LaurentiuMihai99 <naum_mihai2008@yahoo.com>
2021-01-17 14:59:14 +0200

    Added HomePage styles connection

--- anonymousgrading/src/components/HomePage.js
+++ anonymousgrading/src/components/HomePage.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import { withRouter } from 'react-router-dom'
-
+import "./styles/HomePage.css"
 
 class HomePage extends Component{ 
 
     render() { 
         return (
             <div>
-                <h1>{this.props.match.params.userName}</h1>
+                <h1 id="userNameText">{this.props.match.params.userName}</h1>
             </div>
         )
 
