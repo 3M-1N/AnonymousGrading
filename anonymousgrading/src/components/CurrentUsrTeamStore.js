a393c90edfe501610ade83625dda662d25ece7a5
LaurentiuMihai99 <naum_mihai2008@yahoo.com>
2021-01-19 16:03:55 +0200

    Added 'See All Projects' functionality

--- anonymousgrading/src/components/CurrentUsrTeamStore.js
+++ anonymousgrading/src/components/CurrentUsrTeamStore.js
@@ -73,19 +73,6 @@ class CurrentUsrTeamStore extends React.Component{
             this.emitter.emit('ADD_ONE_ERROR')
           }
     }
-    
-
-    async getAllProjects(){
-        try{
-            const response=await fetch(`${SERVER}/projects`)
-            const projects = await response.json()
-            this.projectData=projects
-            this.emitter.emit('GET_PROJECTS_SUCCESS')
-        }catch(err){
-            console.warn(err)
-            this.emitter.emit('GET_PROJECTS_ERROR')
-        }
-    }
 }
 
 export default CurrentUsrTeamStore
--- anonymousgrading/src/components/TeamsPage.js
+++ anonymousgrading/src/components/TeamsPage.js
@@ -1,29 +1,35 @@
 import React, {Component} from 'react'
 import { withRouter } from 'react-router-dom'
-import CurrentUsrTeamStore from './CurrentUsrTeamStore'
+import MemberStore from './MemberStore'
 import ProjectStore from './ProjectStore'
 
 class ProjectsPage extends Component{ 
     constructor(props){
         super(props)
         this.state={
-            projects: []
+            projects: [],
+            members: []
         }
 
         this.projectStore = new ProjectStore()
-        this.teamStore = new CurrentUsrTeamStore()
+        this.memberStore = new MemberStore()
     }
 
     componentDidMount() {
-        this.projectStore.getAll()
-        this.projectStore.emitter.addListener('GET_PROJECTS_SUCCESS',() => {
+        this.projectStore.getAllProjects()
+        this.projectStore.emitter.addListener('GET_ALL_PROJECTS_SUCCESS',() => {
             this.setState({
-                projects: this.projectStore.data
+                projects: this.projectStore.projects
             })
-            
-            //for (var project of this.state.projects) {
-            //    console.log(project[0].description)
-            //}
+            for (var proj of this.state.projects) {
+                console.log("Getting teammates for: " + proj.teamId)
+                this.memberStore.getTeamMembers(proj.teamId)
+            }
+        })
+
+        this.memberStore.emitter.addListener('GET_MEMBERS_SUCCESS',() => {
+            this.state.members.push(this.memberStore.data)
+            console.log("Recieved Members! Check to see if they are correct")
         })
     }
 
